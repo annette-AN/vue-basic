50e538a69b4955ca7966f0757f5cc537fd93134b
AMR <mirye.ann@gmail.com>
2019-07-21 21:06:56 +0900

    login 관련 코드

--- board/src/pages/BoardList.vue
+++ board/src/pages/BoardList.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  async created (){
+    const session = window.applicationSession;
+    
+    const loginResp = await axios.post("http://nineten11.net:7000/api/user/login",{
+      email:"guest",
+      password:""
+    });
+
+    console.log("loginResp", session, loginResp)
+
+    //await axios.get("http://nineten11.net:7000/api/boards")
+    
+  }
+}
+</script>
